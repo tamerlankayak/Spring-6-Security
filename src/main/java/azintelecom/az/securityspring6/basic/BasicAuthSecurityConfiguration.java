0e9cc0b393729c04836956b1612ec0f5d8371975
Tamerlan <tamerlan.mustafayev@rabita.az>
2023-02-17 19:09:34 +0400

    new commit

---
+++ src/main/java/azintelecom/az/securityspring6/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,80 @@
+package azintelecom.az.securityspring6.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableMethodSecurity
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> {
+                    auth
+                            .requestMatchers("/users").hasRole("USER")
+                            .requestMatchers("/admin/**").hasRole("ADMIN")
+                            .anyRequest()
+                            .authenticated();
+                }
+        );
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic();
+        http.csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        var user = User
+                .withUsername("user")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
+
+
+        var admin = User
+                .withUsername("admin")
+//                .password("{noop}dummy")
+                .password("ferrari")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN")
+                .build();
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+
+}
--- src/main/java/azintelecom/az/securityspring6/resources/TodoResources.java
+++ src/main/java/azintelecom/az/securityspring6/resources/TodoResources.java
@@ -1,7 +1,11 @@
 package azintelecom.az.securityspring6.resources;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,8 +16,8 @@ public class TodoResources {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final List<Todo> TODOS_LIST = List.of(
-            new Todo("Tamerlan", "CanoeSprint"),
-            new Todo("Ariz", "Air Force")
+            new Todo("user", "Air Force"),
+            new Todo("admin", "CanoeSprint")
     );
 
     @GetMapping("/todos")
@@ -23,6 +27,10 @@ public class TodoResources {
     }
 
     @GetMapping("/users/{username}/todos")
+    @PreAuthorize("hasRole('USER') and #username == authentication.name")
+    @PostAuthorize("returnObject.username =='user'")
+    @RolesAllowed({"ADMIN", "USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
         return TODOS_LIST.get(0);
     }
